c5afc7ea1c22dd6ab9c57a3148fb19d8d115e76c
Allan Legemaate <alegemaate@gmail.com>
2018-12-02 00:43:41 -0500

    Added Game State

---
+++ include/GameState.h
@@ -0,0 +1,21 @@
+/**
+ * Game State
+ * Allan Legemaate
+ * State of game. Board, turns etc.
+ * 05/11/2018
+**/
+
+#ifndef GAMESTATE_H
+#define GAMESTATE_H
+
+class GameState {
+  public:
+    GameState();
+    virtual ~GameState();
+
+  protected:
+
+  private:
+};
+
+#endif // GAMESTATE_H
